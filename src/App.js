2393408f261e45135b806983033fcb938f131e01
JoeCarlos <jose.ozelzo10@gmail.com>
2020-06-30 13:02:32 -0300

    Estilização do conversor finalizada

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import Conversor from './componets/Conversor'
 function App() {
   return (
     <div className="App">
-      
-      <Conversor moedaA = "USD" moedaB = "BRL">
-
-      </Conversor>
+        <h1>Conversor de moeda</h1>
+        <div className="linha">
+          <Conversor moedaA = "USD" moedaB = "BRL"></Conversor>
+          <Conversor moedaA = "BRL" moedaB = "USD"></Conversor>
+        </div>
+        <div className="linha">
+          <Conversor moedaA = "CAD" moedaB = "BRL"></Conversor>
+          <Conversor moedaA = "BRL" moedaB = "CAD"></Conversor>
+        </div>
+        <div className="linha">
+          <Conversor moedaA = "EUR" moedaB = "BRL"></Conversor>
+          <Conversor moedaA = "BRL" moedaB = "EUR"></Conversor>
+        </div>
     </div>
     
   );
--- src/componets/Conversor.js
+++ src/componets/Conversor.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-export default class Conversor extends Component {
+import "./Conversor.css";
 
+export default class Conversor extends Component {
   constructor(props){
       super(props);
 
